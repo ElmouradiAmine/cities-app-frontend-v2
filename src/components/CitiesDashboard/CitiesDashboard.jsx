61041fa836e0c189069dcb3b1a7bc1d65e3e54d8
Amine Elmouradi <amineelmouradi@mazuki.local>
2021-01-12 22:43:58 +0100

    add error handling
    Update README.md

--- src/components/CitiesDashboard/CitiesDashboard.jsx
+++ src/components/CitiesDashboard/CitiesDashboard.jsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react';
 
 import CitiesList from '../CitiesList/CitiesList';
 import CityInput from '../CityInput/CityInput';
+import Alert from '../Alert/Alert';
+import Spinner from '../Spinner/Spinner';
 
 import './CitiesDashboard.css';
 
@@ -13,15 +15,22 @@ const baseURL = process.env.NODE_ENV === 'production'
 const CitiesDashboard = () => {
   const [cities, setCities] = useState([]);
   const [search, setSearch] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     axios({
       url: `${baseURL}?search=${search}`,
     })
       .then((response) => response.data)
       .then((data) => {
         setCities(data);
-      });
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => setLoading(false));
   }, [search]);
 
   const handleInputSubmit = (text) => {
@@ -30,17 +39,30 @@ const CitiesDashboard = () => {
 
   const getCitiesByCategory = (category) => cities.filter((city) => city.category === category);
 
+  const renderContent = () => {
+    if (loading) {
+      return <Spinner />;
+    }
+    if (error) {
+      return <Alert text="A network error has occured. Please refresh the page or retry later!" type="danger" />;
+    }
+    return (
+      <>
+        <CitiesList
+          cities={getCitiesByCategory('METROPOLE')}
+          title="Villes de métropole"
+        />
+        <CitiesList
+          cities={getCitiesByCategory('OUTRE_MER')}
+          title="Villes d'outre-mer"
+        />
+      </>
+    );
+  };
   return (
     <div className="cities-dashboard">
       <CityInput onSubmit={handleInputSubmit} />
-      <CitiesList
-        cities={getCitiesByCategory('METROPOLE')}
-        title="Villes de métropole"
-      />
-      <CitiesList
-        cities={getCitiesByCategory('OUTRE_MER')}
-        title="Villes d'outre-mer"
-      />
+      {renderContent()}
     </div>
   );
 };
